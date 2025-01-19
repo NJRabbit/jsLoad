b5bf4b2c818ec067699c9ad61a1583df53eb00bc
NJRabbit <jamrosznat@students.cps.k12.in.us>
2025-01-19 04:39:46 +0000

    prepush

---
+++ keyPass/serviceWorker.js
@@ -0,0 +1,10 @@
+const filter = {
+    url: [
+      {
+        urlMatches: 'https://*.*/*',
+      },
+    ],
+  };
+
+
+//chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec)
